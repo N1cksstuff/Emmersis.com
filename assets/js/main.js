2f4d8b44895c21b91dd53c13d70751c0947fe316
Nick <nick.devstuff@gmail.com>
2022-08-21 04:58:40 +0200

    Cleaned up, added merch

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,36 +4,6 @@
 (function($) {
     "use strict";
 
-    //Page cursors
-
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-        t.style.left = n.clientX + "px",
-            t.style.top = n.clientY + "px",
-            e.style.left = n.clientX + "px",
-            e.style.top = n.clientY + "px",
-            i.style.left = n.clientX + "px",
-            i.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-
-    function n(t) {
-        e.classList.add("hover"), i.classList.add("hover")
-    }
-
-    function s(t) {
-        e.classList.remove("hover"), i.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
-
     //Navigation
 
     var app = function() {
@@ -57,18 +27,153 @@
         };
         init();
     }();
-
-
-    //Switch light/dark
-
-    $("#switch").on('click', function() {
-        if ($("body").hasClass("light")) {
-            $("body").removeClass("light");
-            $("#switch").removeClass("switched");
-        } else {
-            $("body").addClass("light");
-            $("#switch").addClass("switched");
-        }
-    });
-
 })(jQuery);
+
+
+//Cursor - Thank you to Curzr!
+
+
+
+class CircleAndDot {
+    constructor() {
+        this.root = document.body
+        this.cursor = document.querySelector(".curzr")
+
+        this.position = {
+                distanceX: 0,
+                distanceY: 0,
+                distance: 0,
+                pointerX: 0,
+                pointerY: 0,
+            },
+            this.previousPointerX = 0
+        this.previousPointerY = 0
+        this.angle = 0
+        this.previousAngle = 0
+        this.angleDisplace = 0
+        this.degrees = 57.296
+        this.cursorSize = 20
+        this.fading = false
+
+        this.cursorStyle = {
+            boxSizing: 'border-box',
+            position: 'fixed',
+            top: `${ this.cursorSize / -2 }px`,
+            left: `${ this.cursorSize / -2 }px`,
+            zIndex: '2147483647',
+            width: `${ this.cursorSize }px`,
+            height: `${ this.cursorSize }px`,
+            backgroundColor: '#fff0',
+            border: '1.25px solid #111920',
+            borderRadius: '50%',
+            boxShadow: '0 -15px 0 -8px #0000',
+            transition: '250ms, transform 100ms',
+            userSelect: 'none',
+            pointerEvents: 'none'
+        }
+
+        this.init(this.cursor, this.cursorStyle)
+    }
+
+    init(el, style) {
+        Object.assign(el.style, style)
+        this.cursor.removeAttribute("hidden")
+
+    }
+
+    move(event) {
+        this.previousPointerX = this.position.pointerX
+        this.previousPointerY = this.position.pointerY
+        this.position.pointerX = event.pageX + this.root.getBoundingClientRect().x
+        this.position.pointerY = event.pageY + this.root.getBoundingClientRect().y
+        this.position.distanceX = this.previousPointerX - this.position.pointerX
+        this.position.distanceY = this.previousPointerY - this.position.pointerY
+        this.distance = Math.sqrt(this.position.distanceY ** 2 + this.position.distanceX ** 2)
+
+        if (event.target.localName === 'button' ||
+            event.target.localName === 'a' ||
+            event.target.onclick !== null ||
+            event.target.className.includes('hover-target')) {
+            this.hover()
+        } else {
+            this.hoverout()
+        }
+
+        this.cursor.style.transform = `translate3d(${this.position.pointerX}px, ${this.position.pointerY}px, 0)`
+
+        this.rotate(this.position)
+        this.fade(this.distance)
+    }
+
+    rotate(position) {
+        let unsortedAngle = Math.atan(Math.abs(position.distanceY) / Math.abs(position.distanceX)) * this.degrees
+        this.previousAngle = this.angle
+
+        if (position.distanceX <= 0 && position.distanceY >= 0) {
+            this.angle = 90 - unsortedAngle + 0
+        } else if (position.distanceX < 0 && position.distanceY < 0) {
+            this.angle = unsortedAngle + 90
+        } else if (position.distanceX >= 0 && position.distanceY <= 0) {
+            this.angle = 90 - unsortedAngle + 180
+        } else if (position.distanceX > 0 && position.distanceY > 0) {
+            this.angle = unsortedAngle + 270
+        }
+
+        if (isNaN(this.angle)) {
+            this.angle = this.previousAngle
+        } else {
+            if (this.angle - this.previousAngle <= -270) {
+                this.angleDisplace += 360 + this.angle - this.previousAngle
+            } else if (this.angle - this.previousAngle >= 270) {
+                this.angleDisplace += this.angle - this.previousAngle - 360
+            } else {
+                this.angleDisplace += this.angle - this.previousAngle
+            }
+        }
+        this.cursor.style.transform += ` rotate(${this.angleDisplace}deg)`
+    }
+
+    hover() {
+        this.cursor.style.border = '10px solid #111920'
+    }
+
+    hoverout() {
+        this.cursor.style.border = '1.25px solid #111920'
+    }
+
+    fade(distance) {
+        this.cursor.style.boxShadow = `0 ${-15 - distance}px 0 -8px #111920, 0 0 0 1px #F2F5F8`
+        if (!this.fading) {
+            this.fading = true
+            setTimeout(() => {
+                this.cursor.style.boxShadow = '0 -15px 0 -8px #11192000, 0 0 0 1px #F2F5F8'
+                this.fading = false
+            }, 50)
+        }
+    }
+
+    click() {
+        this.cursor.style.transform += ` scale(0.75)`
+        setTimeout(() => {
+            this.cursor.style.transform = this.cursor.style.transform.replace(` scale(0.75)`, '')
+        }, 35)
+    }
+
+    remove() {
+        this.cursor.remove()
+    }
+}
+
+(() => {
+    const cursor = new CircleAndDot()
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.onmousemove = function(event) {
+            cursor.move(event)
+        }
+        document.onclick = function() {
+            cursor.click()
+        }
+    } else {
+        cursor.remove()
+    }
+})()
